3036cf5a2080cfdd02da35fd49727088818f60af
Meher143 <krishmeher@msitprogram.net>
2018-09-27 00:34:25 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

---
+++ ADS practice/m3/3-sum/Solution.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.Scanner;
+final class Solution {
+	private Solution() {
+
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int size = sc.nextInt();
+		int []arr = new int[size];
+		for (int i = 0; i < size; i++) {
+			arr[i] = sc.nextInt();
+		}
+		Arrays.sort(arr);
+		int count = 0;
+		for (int i = 0; i < size; i++) {
+			for (int j = i+1; j < size; j++) {
+				int k = Arrays.binarySearch(arr, (arr[i] + arr[j]));
+				if (k > j) {
+					count++;
+				}
+			}
+			System.out.println(count);
+			
+		}
+		}
+		}
+	
+
