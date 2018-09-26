d3285a4e70f347a53e540690769b596045c74f1c
Meher143 <krishmeher@msitprogram.net>
2018-09-27 01:24:58 +0530

    "check_style_score -> 4 of 4 passed. style score: 1/1 "

--- ADS practice/m3/3-sum/Solution.java
+++ ADS practice/m3/3-sum/Solution.java
@@ -4,42 +4,42 @@ import java.util.Scanner;
  * { item_description }.
  */
 final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
-		/**
-		 * { item_description }.
-		 */
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        /**
+         * { item_description }.
+         */
 
-	}
-	/**
-	 * { function_description }.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		/**
-		 * { var_description }.
-		 */
-		Scanner sc = new Scanner(System.in);
-		int size = sc.nextInt();
-		int[] arr = new int[size];
-		for (int i = 0; i < size; i++) {
-			arr[i] = sc.nextInt();
-		}
-		Arrays.sort(arr);
-		int count = 0;
-		for (int i = 0; i < size; i++) {
-			for (int j = i + 1; j < size; j++) {
-				int k = Arrays.binarySearch(arr,
-					-(arr[i] + arr[j]));
-				if (k > j) {
-					count++;
-				}
-			}
-		}
-			System.out.println(count);
-		}
-	}
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        /**
+         * { var_description }.
+         */
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        int count = 0;
+        for (int i = 0; i < size; i++) {
+            for (int j = i + 1; j < size; j++) {
+                int k = Arrays.binarySearch(arr,
+                    -(arr[i] + arr[j]));
+                if (k > j) {
+                    count++;
+                }
+            }
+        }
+            System.out.println(count);
+        }
+    }
 
