dda891ab6b25eb4dbac9baf7de1b4b49e4c797ff
Meher143 <krishmeher@msitprogram.net>
2018-09-30 15:53:08 +0530

    "check_style_score -> 0 of 12 passed. style score: 0/1 "

--- ADS Assignments/m6/Assignment-1/Solution.java
+++ ADS Assignments/m6/Assignment-1/Solution.java
@@ -1,94 +1,86 @@
 import java.util.Scanner;
 import java.util.Arrays;
-class LinkedList<E> {
-    class Node {
-        E data;
-        Node next = null;
-    }
+class Node {
+	String data;
+	Node next;
+	Node() {
 
-    int size = 0;
-    Node head;
-    public void insertAt(E data, int pos) {
-        Node newNode = new Node();
-        if (pos > size + 1 || pos < 0) return;
-        if (size == 0) {
-            head.data = data;
-            return;
-        }
-        if (pos == 0) {
-            newNode.data = data;
-            newNode.next = head;
-            head = newNode;
-            size++;
-            return;
-        }
-        Node temp = head;
-        int tcount = 0;
-        while (tcount++ < pos - 1)
-            temp = temp.next;
-        newNode.data = data;
-        newNode.next = temp.next;
-        temp.next = newNode;
-        size++;
-    }
-    public void insertEnd(E data) {
-        insertAt(data, size);
-    }
-    public void delAt(int pos) {
-        if(pos > size-- && pos < 0) return;
-        Node temp = head;
-        int tcount = 0;
-        if(pos == 0) {
-            head.next = null;
-            return;
-        }
-        while(tcount++ < pos - 1)
-            temp = temp.next;
-        temp.next = temp.next.next;
-    }
-    public void delEnd() {
-        delAt(size - 1);
-    }
-    public boolean isEmpty() {
-        return size == 0;
-    }
-    public E dataAt(int pos) {
-        if(pos > size && pos < 0) return null;
-        Node temp = head;
-        int tcount = 0; 
-        while(tcount++ < pos)
-            temp = temp.next;
-        return temp.data;
-    }
-    public E dataEnd() {
-        return dataAt(size -1);
-    }
-    // public void 
-    public void print() {
-        Node temp = head;
-        int tcount = 0;
-        while (tcount++ < size) {
-            System.out.print(temp.data + " ");
-            temp  = temp.next;
-        }
-    }
+	}
+	Node(String data){
+		this.data = data;
+	}
 }
+class LinkedList {
+	Node start;
+	int size;
+	LinkedList() {
+     start = new Node();
+     size = 0;
+	}
+	void insert(String item) {
+		Node obj = new Node();
+		obj.data = item;
+		if (size == 0) {
+			start = obj;
+			size++;
+			return;
+		}
+		obj.next = start;
+		start = obj;
+	}
+	String delete() {
+		String data = start.data;
+		start = start.next;
+		size--;
+		return data;
+	}
+	boolean isEmpty() {
+		if (size == 0) {
+			return true;
+		}
+		return false;
+	}
+	int size() {
+		return size;
+	}
+}
+class Stack {
+	LinkedList l1;
+	Stack () {
+		l1 = new LinkedList();
+	}
+	void push(String item) {
+		l1.insert(item);
+	}
+	String pop() {
+		return l1.delete();
+	}
+	boolean isEmpty() {
+		return l1.isEmpty();
+	}
+	int size() {
+		return l1.size();
+	}
+}
+
 class AddLargeNumbers {
       private static LinkedList lobj = new LinkedList();
     
     public static LinkedList numberToDigits(String number) {
-        for (int i = 0; i < number.length; i++) {
-            System.out.println(999);
-            System.out.println(111);
-            return size;
+        LinkedList object = new LinkedList();
+        for (int i = 0; i < number.length(); i++) {
+        }
+        System.out.println(object);
+        return object;
+
 }
 
 public static String digitsToNumber(LinkedList list) {
-
+return null;
     }
 
 public static LinkedList addLargeNumbers(LinkedList list1, LinkedList list2) {
-
+return null;
     }
 }
 
