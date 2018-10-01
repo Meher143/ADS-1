2db5a19663cb0402011003e07c190ffd3b75d894
Meher143 <krishmeher@msitprogram.net>
2018-10-01 19:31:20 +0530

    "check_style_score -> 2 of 2 passed. style score: 0/1 "

--- ADS Assignments/m7/7.1 Steque/Solution.java
+++ ADS Assignments/m7/7.1 Steque/Solution.java
@@ -1,4 +1,4 @@
-import java.util.*;
+import java.util.Scanner;
 class Node {
 	String data;
 	Node next;
@@ -8,11 +8,10 @@ class Node {
 }
 class Steque {
 	Node start, end;
-	int size;
+	int size = 0;
 	Steque() {
 		start = null;
 		end = null;
-		size = 0;
 	}
 	void push(String item) {
 		Node obj = new Node(item);
@@ -38,44 +37,45 @@ class Steque {
 		end.next = obj;
 		end = obj;
 		size++;
-
 	}
-	String pop()throws Exception {
+	String pop() throws Exception {
 		if (isEmpty()) {
 			throw new Exception("Steque is empty.");
 		}
-       String data = start.data;
-       start = start.next;
-       size--;
-       return data;
+		String data = start.data;
+		start = start.next;
+		size--;
+		return data;
 	}
 	boolean isEmpty() {
 		return size == 0;
 	}
 	public String toString() {
+		// if(isEmpty()){
+
+		// }
 		Node temp = start;
 		String res = "";
-		while(temp!=null) {
-			res += temp.data + ",";
+		while (temp != null) {
+			res += temp.data + ", ";
 			temp = temp.next;
 		}
-		return res.substring(0,res.length()-2);
+		return res.substring(0, res.length() - 2);
 	}
 }
-
 class Solution {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int num = Integer.parseInt(sc.nextLine());
 		Steque st = new Steque();
-		while(sc.hasNextLine()) {
+		while (sc.hasNextLine()) {
 			String input[] = sc.nextLine().split(" ");
 			switch (input[0]) {
-				case "push":
+			case "push":
 				st.push(input[1]);
 				System.out.println(st);
 				break;
-				case "pop":
+			case "pop":
 				try {
 					st.pop();
 					if (st.isEmpty()) {
@@ -83,12 +83,17 @@ class Solution {
 					} else {
 						System.out.println(st);
 					}
-				} catch(Exception e) {
+				} catch (Exception e) {
 					System.out.println(e.getMessage());
-				}				
+				}
 				break;
-				case "enqueue":
+			case "enqueue":
 				st.enqueue(input[1]);
+				System.out.println(st);
+				break;
+			default:
+				st = new Steque();
+				System.out.println();
 				break;
 			}
 		}
