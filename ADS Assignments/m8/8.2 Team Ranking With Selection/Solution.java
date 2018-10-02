2ee03eff473126d2cf256a44368cd0de26eef4a1
Meher143 <krishmeher@msitprogram.net>
2018-10-02 23:37:47 +0530

    "check_style_score -> 6 of 6 passed. style score: 0/1 "

--- ADS Assignments/m8/8.2 Team Ranking With Selection/Solution.java
+++ ADS Assignments/m8/8.2 Team Ranking With Selection/Solution.java
@@ -23,7 +23,7 @@ final class Solution {
                 str[2], str[2 + 1]);
             team.addCricketTeam(cric);
         }
-        //team.selectionSort();
+        team.selectionSort();
         System.out.println(team);
     }
 }
