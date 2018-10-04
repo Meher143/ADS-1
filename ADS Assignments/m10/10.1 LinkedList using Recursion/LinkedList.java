e2d586edb60ab6b89d75bc6abc519f6eb29d655b
Meher143 <krishmeher@msitprogram.net>
2018-10-04 23:29:00 +0530

    "ads1_m10_a1: 3/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

---
+++ ADS Assignments/m10/10.1 LinkedList using Recursion/LinkedList.java
@@ -0,0 +1,90 @@
+/**
+ * Class for node.
+ */
+class Node {
+    int data;
+    Node next;
+    Node(int data) {
+        this.data = data;
+    }
+}
+/**
+ * List of linkeds.
+ */
+class LinkedList {
+    Node head;
+    int size;
+    /**
+     * Constructs the object.
+     */
+    LinkedList() {
+        head = null;
+        size = 0;
+    }
+    /**
+     * insertAt method.
+     *
+     * @param      pos        The position
+     * @param      ele        The ele
+     *
+     * @throws     Exception  e
+     */
+    public void insertAt(int pos, int ele) throws Exception{
+        Node newNode = new Node(ele);
+        if (pos < 0 || pos > size) {
+            throw new Exception();
+        }
+        head = insertAt(pos, head, newNode, 0);
+    }
+    /**
+     * Oveloading insertAt method.
+     *
+     * @param      pos    The position
+     * @param      first  The first
+     * @param      obj    The object
+     * @param      count  The count
+     *
+     * @return     Node
+     */
+    public Node insertAt(int pos, Node first, Node obj, int count) {
+        if (pos == count) {
+            obj.next = first;
+            size++;
+            return obj;
+        }
+        first.next = insertAt(pos, first.next, obj, count + 1);
+        return first;
+    }
+    /**
+     * reverse method.
+     */
+    public void reverse() {
+        reverse(null, head);
+    }
+    /**
+     * Overloading reverse method.
+     *
+     * @param      previous  The previous
+     * @param      current   The current
+     */
+    void reverse(Node previous, Node current) {
+        if (current != null) {
+            reverse(current, current.next);
+            current.next = previous;
+        } else {
+            head = previous;
+        }
+    }
+    /**
+     * display method.
+     */
+    void display() {
+        Node temp = head;
+        String str = "";
+        while (temp != null) {
+            str += temp.data+", ";
+            temp = temp.next;
+        }
+        System.out.println(str.substring(0,str.length()-2));
+    }
+}
