d7a04ec83c35574f9f76eb19600d8972a43d745d
Meher143 <krishmeher@msitprogram.net>
2018-10-09 23:37:54 +0530

    "ads1_m14_a1: 4/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS Assignments/m14/14.1 Dynamic Median Finding/Solution.java
+++ ADS Assignments/m14/14.1 Dynamic Median Finding/Solution.java
@@ -87,12 +87,13 @@ public class Solution {
         int leftNode = 2 * i + 1;
         int rightNode = 2 * i + 2;
         int largestNumber = i;
-        if 
-            (leftNode < heapSize && inputArray[leftNode] > inputArray[largestNumber]) {
+        if (leftNode < heapSize 
+            && inputArray[leftNode] > inputArray[largestNumber]) {
             largestNumber = leftNode;
         }
         if 
-            (rightNode < heapSize && inputArray[rightNode] > inputArray[largestNumber]) {
+            (rightNode < heapSize 
+                && inputArray[rightNode] > inputArray[largestNumber]) {
             largestNumber = rightNode;
         }
         if (largestNumber != i) {
@@ -124,10 +125,12 @@ public class Solution {
         int leftNode = 2 * i + 1;
         int rightNode = 2 * i + 2;
         int smallest = i;
-        if (leftNode < heapSize && inputArray[leftNode] < inputArray[smallest]) {
+        if (leftNode < heapSize 
+            && inputArray[leftNode] < inputArray[smallest]) {
             smallest = leftNode;
         }
-        if (rightNode < heapSize && inputArray[rightNode] < inputArray[smallest]) {
+        if (rightNode < heapSize 
+            && inputArray[rightNode] < inputArray[smallest]) {
             smallest = rightNode;
         }
         if (smallest != i) {
