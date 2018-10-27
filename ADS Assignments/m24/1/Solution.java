f0eac855b8ba6016dfda20c806db7de02841ef7c
Meher143 <krishmeher@msitprogram.net>
2018-10-27 12:45:05 +0530

    "m24_a1_1: 0/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS Assignments/m24/1/Solution.java
+++ ADS Assignments/m24/1/Solution.java
@@ -1,30 +1,78 @@
 import java.util.Scanner;
+/**
+ * Class for student details.
+ */
 class StudentDetails {
+	/**
+	 * { var_description }.
+	 */
 	private String rollnumber;
+	/**
+	 * { var_description }.
+	 */
 	private String name;
+	/**
+	 * { var_description }.
+	 */
 	private double totalmarks;
+	/**
+	 * Constructs the object.
+	 */
 	StudentDetails() {
 
 	}
+	/**
+	 * Constructs the object.
+	 *
+	 * @param      rollno     The rollno
+	 * @param      name_1     The name 1
+	 * @param      totalmark  The totalmark
+	 */
 	StudentDetails(final String rollno, final String name_1, final double totalmark) {
 		this.rollnumber = rollno;
 		this.name = name_1;
 		this.totalmarks = totalmark;
 	}
+	/**
+	 * Gets the name.
+	 *
+	 * @return     The name.
+	 */
 	public String getName() {
 		return name;
 	}
+	/**
+	 * { function_description }
+	 *
+	 * @return     { description_of_the_return_value }.
+	 */
 	public String rollno() {
 		return rollnumber;
 	}
+	/**
+	 * { function_description }.
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
 	public double totalmark() {
 		return totalmarks;
 	}
 }
+/**
+ * { item_description }.
+ */
 final class Solution {
+	/**
+	 * Constructs the object.
+	 */
 	private Solution() {
 
 	}
+	/**
+	 * { function_description }
+	 *
+	 * @param      args  The arguments
+	 */
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		StudentDetails s = new StudentDetails();
